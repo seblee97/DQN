b6117fb0ed2a4bf11b0c359134905fad075e3534
t-sel <t-sel@microsoft.com>
2018-11-27 14:00:16 +0000

    first commit

---
+++ dqn.py
@@ -0,0 +1,22 @@
+import torch
+import numpy as np 
+import torch.nn.functional as F
+from torch import nn
+
+import gym
+
+class QNetwork(nn.module):
+
+    def __init__(self, obs_space, num_actions):
+        nn.Module.__init__(self)
+        self.obs_space = obs_space
+        self.num_actions = num_actions
+
+    def forward(self, obs):
+
+
+
+class DQN:
+
+    def __init__(self):
+
