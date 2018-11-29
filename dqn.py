30798a0f12f48539ccb4b5ab8c43f187f7dbfca1
t-sel <t-sel@microsoft.com>
2018-11-29 11:08:54 +0000

    progress. QNetwork layers implemented, need to sort out dimensions.

--- dqn.py
+++ dqn.py
@@ -3,22 +3,26 @@ import numpy as np
 import torch.nn.functional as F
 from torch import nn
 import random
+import PIL
+from torchvision import transforms
 
 import gym
 import gym_tetris
 
-"""""""""""""""""""
 # Tetris action space: Discrete(12) no-action, left, right, down, rotate-left, rotate-right, left+down, 
 #                                   right+down, left+rotate-left, right+rotate-right, left+rotate-right, right+rotate-left
-"""""""""""""""""""
 
 config = {
     "env": gym_tetris.make('Tetris-v0'),
     "replay_buffer_size": 1000,
     "epsilon": 0.05,
     "gamma": 0.95,
-    "batch_size": 30
+    "batch_size": 30,
+    "processed_state_dims": (100, 78)
 }
+
+def _array_to_image(rgb_state):
+    return PIL.Image.fromarray(rgb_state)
 
 def trial_env():
     env = gym_tetris.make('Tetris-v0')
@@ -31,32 +35,46 @@ def trial_env():
     return env
 
 
-class FeatureEncoder(nn.Module):
+class PreProcessor(nn.Module):
 
     """
-    feature encoder to preprocess state observation
+    preprocess state observation: grayscale + downsample
     """
 
-    def __init__(self, obs_space):
+    def __init__(self, output_size=(84,84)):
         nn.Module.__init__(self)
-        self.obs_space = obs_space
+        self.transform = transforms.Compose([
+            transforms.Resize(output_size),
+            transforms.Grayscale(),
+            transforms.ToTensor()
+        ])
     
-    def forward(self, obs):
-        return None
+    def forward(self, state):
+        pil = _array_to_image(state)
+        processed_state = self.transform(pil)
+        return processed_state
 
 class QNetwork(nn.Module):
 
-    def __init__(self, obs_space, num_actions):
+    def __init__(self, obs_space=((4,84,84)), num_actions=12):
         nn.Module.__init__(self)
         self.obs_space = obs_space
         self.num_actions = num_actions
+        self.conv1 = nn.Conv2d(self.obs_space[0], out_channels=16, kernel_size=8, stride=4)
+        self.conv2 = nn.Conv2d(16, out_channels=32, kernel_size=4, stride=2)
+        self.lin = nn.Linear(32, num_actions)
 
-    def forward(self, obs, action):
 
-        return None
+    def forward(self, obs):
+        raise ('obs_shape', obs.size())
+        x = F.relu(self.conv1(obs))
+        print('x', x.size())
+        x = F.relu(self.conv2(x))
+        x = self.lin(x)
+        return x
 
     def compute_actions(self, obs):
-        amax = np.argmax([self.forward(obs, a) for a in #])
+        amax = np.argmax([self.forward(obs, a) for a in A])
 
 
 class DQN:
@@ -67,7 +85,8 @@ class DQN:
         self.env = self.config["env"]
         self.action_space = self.env.action_space
 
-        self.feature_encoder = FeatureEncoder(OBSSPACE)
+        self.processed_size = self.config["processed_size"]
+        self.preprocessor = PreProcessor(self.processed_size)
         self.qnetwork = QNetwork(OBSSPACE, NUMACTIONS)
         
         self.replay_buffer_size = self.config["replay_buffer_size"]
@@ -93,7 +112,7 @@ class DQN:
             else:
                 raise NotImplementedError #ComputeActions
             new_state, reward, done, info = self.step_env(act)
-            experience = (self.feature_encoder(state), action, reward, self.feature_encoder(new_state))
+            experience = (self.preprocessor(state), action, reward, self.preprocessor(new_state))
             self.replay_buffer.add(experience)
             state = new_state
             if i >= self.batch_size:
@@ -106,7 +125,7 @@ class DQN:
         if done:
             y = reward
             print("Episode finished after {} timesteps".format(i+1)) 
-            break
+            #break
         else:
             y = reward + self.gamma*np.amax([self.qnetwork(OBSSPACE, NUMACTIONS) for a in ACTIONS])
         loss = LOSSFN(y, state, action)
